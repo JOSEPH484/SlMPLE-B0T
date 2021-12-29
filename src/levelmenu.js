b97fe8ad869b37272521f45dc9da56d8f0468af9
JOSEPH484 <96081332+JOSEPH484@users.noreply.github.com>
2021-12-29 18:37:38 -0500

    Update levelmenu.js

--- src/levelmenu.js
+++ src/levelmenu.js
@@ -1,33 +1,12 @@
 const levelmenu = (prefix, pushname) => {
-    return `
+    return `🔱 𝐁𝐈𝐍𝐄𝐑𝐈𝐀 𝐔𝐍𝐈𝐕𝐄𝐑𝐒𝐀𝐋:
 
-*Comandos De Level 🆕 beta*
- 
-══════════════
+⚜ 𝑪𝒉𝒂𝒏𝒏𝒆𝒍: https://t.me/BineriaUniversal
+⚜ 𝑪𝒉𝒂𝒕: https://t.me/BineriaUniversal0
+⚜ 𝑮𝒓𝒖𝒑𝒐 𝑶𝒇𝒊𝒄𝒊𝒂𝒍: https://chat.whatsapp.com/DwvnxoVjSoz4246vFbZBCP
+⚜ 𝑮𝒓𝒖𝒑𝒐 𝑨𝒍𝒊𝒂𝒅𝒐: https://chat.whatsapp.com/Bhi2QNyfiCQ75thf9llSDl
 
-PARA ACTIVARLO EL BOT NECESITA DE ADMINISTRACIÓN
-
-*PARA ACTIVAR LEVEL*
-
-*leveling 1
-
-
-*PARA DESACTIVAR LEVEL*
-
-*leveling 0
-
-══════════════
-
-PARA VER TU XP EN TIEMPO REAL
-
-*level
-
-══════════════
-    
-*Recuerda que si actualizas tu bot o borras el numero del bot o otra cosa todos los levels de los particiantes quedaran en 0*
-    
-_*by shanduy*_
-`
+🎩 𝑶𝒘𝒏𝒆𝒓: 𝗝𝗔𝗦𝗜𝗘𝗟 𝗔𝗡𝗗 𝗝𝗢𝗘𝗟`
 
 }
 
