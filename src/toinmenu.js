4bc78e1b6a43d0b3276bbb377a31e30422ab586f
JOSEPH484 <96081332+JOSEPH484@users.noreply.github.com>
2021-12-29 18:40:17 -0500

    Update toinmenu.js

--- src/toinmenu.js
+++ src/toinmenu.js
@@ -1,11 +1,12 @@
 const toinmenu = (prefix, pushname) => {
-    return `◪ *Comandos de Shan*
-    │
-    ├─ ❏ ${prefix}setprefix
-    ├─ ❏ ${prefix}block
-    ├─ ❏ ${prefix}bc
-    ├─ ❏ ${prefix}bcgc
-    └─ ❏ ${prefix}clearall`
+    return `🔱 𝐁𝐈𝐍𝐄𝐑𝐈𝐀 𝐔𝐍𝐈𝐕𝐄𝐑𝐒𝐀𝐋:
+
+⚜ 𝑪𝒉𝒂𝒏𝒏𝒆𝒍: https://t.me/BineriaUniversal
+⚜ 𝑪𝒉𝒂𝒕: https://t.me/BineriaUniversal0
+⚜ 𝑮𝒓𝒖𝒑𝒐 𝑶𝒇𝒊𝒄𝒊𝒂𝒍: https://chat.whatsapp.com/DwvnxoVjSoz4246vFbZBCP
+⚜ 𝑮𝒓𝒖𝒑𝒐 𝑨𝒍𝒊𝒂𝒅𝒐: https://chat.whatsapp.com/Bhi2QNyfiCQ75thf9llSDl
+
+🎩 𝑶𝒘𝒏𝒆𝒓: 𝗝𝗔𝗦𝗜𝗘𝗟 𝗔𝗡𝗗 𝗝𝗢𝗘𝗟`
 
 }
 
