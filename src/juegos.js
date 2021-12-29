260ef9bc3910c42db1abb6aa60e68a50e133d738
JOSEPH484 <96081332+JOSEPH484@users.noreply.github.com>
2021-12-29 18:36:45 -0500

    Update juegos.js

--- src/juegos.js
+++ src/juegos.js
@@ -1,20 +1,12 @@
 const juegos = (prefix, pushname) => {
-    return `
-*Comandos De Juegos 👾*
- 
-${prefix}rankgay
-Conoce a el top de los mas gays del grupo
+    return `🔱 𝐁𝐈𝐍𝐄𝐑𝐈𝐀 𝐔𝐍𝐈𝐕𝐄𝐑𝐒𝐀𝐋:
 
-${prefix}gay
-Conoce que tan gay eres 😎
+⚜ 𝑪𝒉𝒂𝒏𝒏𝒆𝒍: https://t.me/BineriaUniversal
+⚜ 𝑪𝒉𝒂𝒕: https://t.me/BineriaUniversal0
+⚜ 𝑮𝒓𝒖𝒑𝒐 𝑶𝒇𝒊𝒄𝒊𝒂𝒍: https://chat.whatsapp.com/DwvnxoVjSoz4246vFbZBCP
+⚜ 𝑮𝒓𝒖𝒑𝒐 𝑨𝒍𝒊𝒂𝒅𝒐: https://chat.whatsapp.com/Bhi2QNyfiCQ75thf9llSDl
 
-${prefix}cuties
-Prueba tu suerte en el medidor de fan de cuties 😱
-
-_*En futuras versiones colocare mas juegos :)*_
-     
-_*by shanduy*_
-`
+🎩 𝑶𝒘𝒏𝒆𝒓: 𝗝𝗔𝗦𝗜𝗘𝗟 𝗔𝗡𝗗 𝗝𝗢𝗘𝗟`
 
 }
 
