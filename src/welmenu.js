cfa8ec251f687c4164a32f4813c6ec893b4c388e
JOSEPH484 <96081332+JOSEPH484@users.noreply.github.com>
2021-12-29 18:41:09 -0500

    Update welmenu.js

--- src/welmenu.js
+++ src/welmenu.js
@@ -1,19 +1,12 @@
 const welmenu = (prefix, pushname) => {
-    return `
-*Comandos De Bienvenida 👋*
+    return `🔱 𝐁𝐈𝐍𝐄𝐑𝐈𝐀 𝐔𝐍𝐈𝐕𝐄𝐑𝐒𝐀𝐋:
 
-*PARA ACTIVAR LA BIENVENIDA*
+⚜ 𝑪𝒉𝒂𝒏𝒏𝒆𝒍: https://t.me/BineriaUniversal
+⚜ 𝑪𝒉𝒂𝒕: https://t.me/BineriaUniversal0
+⚜ 𝑮𝒓𝒖𝒑𝒐 𝑶𝒇𝒊𝒄𝒊𝒂𝒍: https://chat.whatsapp.com/DwvnxoVjSoz4246vFbZBCP
+⚜ 𝑮𝒓𝒖𝒑𝒐 𝑨𝒍𝒊𝒂𝒅𝒐: https://chat.whatsapp.com/Bhi2QNyfiCQ75thf9llSDl
 
-- ${prefix}welcome 1
-
-*PARA DESACTIVAR LA BIENVENIDA*
-
-- ${prefix}welcome 0
-
-_Para activar este comando el bot necesita tener admin_
-
-_*by shanduy*_
-`
+🎩 𝑶𝒘𝒏𝒆𝒓: 𝗝𝗔𝗦𝗜𝗘𝗟 𝗔𝗡𝗗 𝗝𝗢𝗘𝗟`
 
 }
 
