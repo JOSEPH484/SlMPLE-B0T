edc3f6b1aef794f40344979e551fa9b13fa89dae
JOSEPH484 <96081332+JOSEPH484@users.noreply.github.com>
2021-12-29 18:39:27 -0500

    Update otak.js

--- src/otak.js
+++ src/otak.js
@@ -1,17 +1,12 @@
 const otak = (prefix, pushname) => {
-    return `*Palabras especificas para que el bot interactue con ustedes mis queridos otakus*
+    return `🔱 𝐁𝐈𝐍𝐄𝐑𝐈𝐀 𝐔𝐍𝐈𝐕𝐄𝐑𝐒𝐀𝐋:
 
+⚜ 𝑪𝒉𝒂𝒏𝒏𝒆𝒍: https://t.me/BineriaUniversal
+⚜ 𝑪𝒉𝒂𝒕: https://t.me/BineriaUniversal0
+⚜ 𝑮𝒓𝒖𝒑𝒐 𝑶𝒇𝒊𝒄𝒊𝒂𝒍: https://chat.whatsapp.com/DwvnxoVjSoz4246vFbZBCP
+⚜ 𝑮𝒓𝒖𝒑𝒐 𝑨𝒍𝒊𝒂𝒅𝒐: https://chat.whatsapp.com/Bhi2QNyfiCQ75thf9llSDl
 
-Quien es tu sempai botsito
-Me gimes 7u7
-Te amo botsito uwu
-Onichan
-La toca 7w7
-
-
-_*Ojito escribe tal como esta*_
-
-ву ѕнαη∂υу`
+🎩 𝑶𝒘𝒏𝒆𝒓: 𝗝𝗔𝗦𝗜𝗘𝗟 𝗔𝗡𝗗 𝗝𝗢𝗘𝗟`
 
 }
 
