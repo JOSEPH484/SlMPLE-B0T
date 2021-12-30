546603155fb32b2aa021c499d114a86aa3a96666
JOSEPH484 <96081332+JOSEPH484@users.noreply.github.com>
2021-12-29 22:53:47 -0500

    Update version.js

--- src/version.js
+++ src/version.js
@@ -1,32 +1,11 @@
 const version = (prefix, pushname) => {
     return `
-*ShanBot by shanduy*
+*✨ JosephBot ✨*
 
-*Actualizado:* 03 de Diciembre del 2021
-*Versión actual:* 4.4
-*Ofrecida por:* shanduy™
-
-
-*INFORME*
-
-Si no funciona el comando *play o *play2 checa el blog del bot y mira la version que poses 
-Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
-Cambiare la apikey pasando un mes o si veo muchos atercados o robos
-Si vez a alguien que robo mi bot comunicate conmigo con el comando *creador
-
-
-*COMO ACTUALIZAR EL BOT*
-
-https://cutt.ly/dWp5uqE
-
-
-*SIGUEME EN INSTAGRAM 🥸🧉*
-
-https://www.instagram.com/thepavos
-
-
-_*by shanduy*_
-`
+*Actualizado:* 29 de Diciembre del 2021
+*Versión actual:* 4.0
+*Ofrecido por:* Joseph
+*Créditos: https://github.com/adiwajshing`
 
 }
 
